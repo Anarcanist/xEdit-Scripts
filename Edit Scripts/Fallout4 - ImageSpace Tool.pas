abb1cca636b200d67ca5fa0613d6d81a08fb2c35
Daniel Davis <25163520+Anarcanist@users.noreply.github.com>
2019-08-06 15:20:29 -0400

    Update and repurpose
    Clarified the explicit uses, added newer features and updated for the more stringent xEdit syntax checks.

--- Edit Scripts/Fallout4 - ImageSpace Tool.pas
+++ Edit Scripts/Fallout4 - ImageSpace Tool.pas
@@ -1,40 +1,54 @@
 {
-  Completely cleans out ImageSpace records of all tonemapping effects. Optionally,
-  you can set Depth of Field to effect the sky (for smooth transition) at Radius 7.
-  Technically you can change the values to make your own tonemapping setup for the
-  entire game, if that's what you're looking to do. Just change the numbers at the
-  end of the lines to what seems reasonable; I won't be documenting their impact.
+  This tool is used to alter ImageSpace records, primarily to
+  remove tinting or unwanted changes in tonemapping across cells
+  in Fallout 4. Many of the settings are commented out to stop
+  user error from causing irreparable damage to their ImageSpace
+  records.
 
-  Because I know some people would want it (though I wouldn't use it) I have put the
-  proper values to disable distant Depth of Field altogether. I prefer the long and
-  soft approach Fallout 76 uses, but if you want no blur, uncomment those lines.
+  To activate any of the features, merely remove the // comment
+  before the code, then change the numbers to your desire. I will
+  eventually learn to create a GUI so this isn't necessary, but
+  for the time being this was the quick and easy way for my uses.
+
+  WARNING: Using RemoveElement LUT is not easily reversible in
+  script format. You will have to manually readd it so that the
+  element can be set for each ImageSpace record. Use only if you
+  know exactly what it's good for.
+    
+    Changelog - 1.1.0
+    - Corrected signature check for xEdit updates
+    - Added processing message to show progress in records
+    - Redid description to more accurately represent purpose
 
   - Anarcanist / Spellshade
     http://www.github.com/anarcanist
 }
-unit userscript;
+unit UserScript;
 function Process(e: IInterface): integer;
 begin
     if Signature(e) <> 'IMGS' then
-        AddMessage('This is not an ImageSpace!');
         Exit;
-    Result := 0;
-        SetElementNativeValues(e, 'HNAM - HDR\Bloom Threshold', 1.0);
-        SetElementNativeValues(e, 'HNAM - HDR\Bloom Scale', 0.0);
-        SetElementNativeValues(e, 'HNAM - HDR\Eye Adapt Speed', 1.0);
-        SetElementNativeValues(e, 'HNAM - HDR\Auto Exposure Max', 1.0);
-        SetElementNativeValues(e, 'HNAM - HDR\Auto Exposure Min', 0.0);
-        SetElementNativeValues(e, 'HNAM - HDR\Middle Gray', 0.5);
-        SetElementNativeValues(e, 'HNAM - HDR\Tonemap E', 0.0);
-        SetElementNativeValues(e, 'CNAM - Cinematic\Saturation', 1.0);
-        SetElementNativeValues(e, 'CNAM - Cinematic\Brightness', 1.0);
-        SetElementNativeValues(e, 'CNAM - Cinematic\Contrast', 1.0);
-        SetElementNativeValues(e, 'TNAM - Tint\Amount', 0.0);
-        //  SetElementNativeValues(e, 'DNAM - Depth of Field\Sky / Blur Radius', 17000);
-        //  SetElementNativeValues(e, 'DNAM - Depth of Field\Strength', 0.0);
-        //  SetElementNativeValues(e, 'DNAM - Depth of Field\Distance', 250000);
-        //  SetElementNativeValues(e, 'DNAM - Depth of Field\Range', 500000);
-        RemoveElement(e, 'TX00 - LUT');
+
+        AddMessage('Processing: ' + FullPath(e));
+        //SetElementEditValues(e, 'HNAM - HDR\Eye Adapt Speed', 3.0);
+        //SetElementEditValues(e, 'HNAM - HDR\Auto Exposure Max', 3.00);
+        //SetElementEditValues(e, 'HNAM - HDR\Auto Exposure Min', 1.50);
+        SetElementEditValues(e, 'HNAM - HDR\Middle Gray', 0.18);
+        SetElementEditValues(e, 'HNAM - HDR\Tonemap E', 0.02);
+        SetElementEditValues(e, 'HNAM - HDR\Bloom Threshold', 1.0);
+        SetElementEditValues(e, 'HNAM - HDR\Bloom Scale', 0.0);
+        //SetElementEditValues(e, 'HNAM - HDR\Sunlight Scale', 1.0);
+        //SetElementEditValues(e, 'HNAM - HDR\Sky Scale', 1.0);
+        SetElementEditValues(e, 'CNAM - Cinematic\Saturation', 1.0);
+        SetElementEditValues(e, 'CNAM - Cinematic\Brightness', 1.0);
+        SetElementEditValues(e, 'CNAM - Cinematic\Contrast', 1.0);
+        SetElementEditValues(e, 'TNAM - Tint\Amount', 0.0);
+        //SetElementEditValues(e, 'DNAM - Depth of Field\Sky / Blur Radius', 17000);
+        //SetElementEditValues(e, 'DNAM - Depth of Field\Strength', 0.5);
+        //SetElementEditValues(e, 'DNAM - Depth of Field\Distance', 5000);
+        //SetElementEditValues(e, 'DNAM - Depth of Field\Range', 75000);
+        //SetElementEditValues(e, 'TX00 - LUT', 'YOUR LUT FILE PATH HERE');
+        //RemoveElement(e, 'TX00 - LUT');
     AddMessage('Processing: ' + FullPath(e));
 end;
 end.
